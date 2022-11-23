1a7d1c979a2f25ed586b34c6712db9acd40611fb
Patrik Smeds <patrik.smeds@gmail.com>
2022-11-23 16:09:53 +0100

    feat: first commit

---
+++ bclconvert/handlers/bclconvert_handlers.py
@@ -0,0 +1,317 @@
+# This file has been modified from the https://github.com/arteria-project/arteria-bcl2fastq repo
+# bcl2fastq/handlers/bcl2fastq_handlers.py
+import json
+import logging
+import os
+
+from bclconvert.lib.jobrunner import LocalQAdapter
+from bclconvert.lib.bclconvert_utils import BclConvertRunnerFactory, BclConvertConfig
+from bclconvert import __version__ as version
+from bclconvert.lib.bclconvert_logs import BclConvertLogFileProvider
+from arteria.exceptions import ArteriaUsageException
+from arteria.web.state import State
+from arteria.web.handlers import BaseRestHandler
+
+
+log = logging.getLogger(__name__)
+
+class BclConvertServiceMixin:
+    """
+    Provides bclconvert related services that can be mixed in.
+    It will create adaptors to the runner service the first time a
+    request is made and then keep that adaptor. These adaptors are static,
+    so that only one such adaptor is created for the entire application.
+    """
+
+    _runner_service = None
+
+    @staticmethod
+    def runner_service():
+        """
+        Create an adaptor to the runner service unless one already exists
+        """
+        if BclConvertServiceMixin._runner_service:
+            return BclConvertServiceMixin._runner_service
+        else:
+            import multiprocessing
+            nbr_of_cores = multiprocessing.cpu_count()
+            # TODO Make configurable
+            BclConvertServiceMixin._runner_service = LocalQAdapter(nbr_of_cores=nbr_of_cores, interval=2)
+            return BclConvertServiceMixin._runner_service
+
+    _bclconvert_cmd_generation_service = None
+
+    @staticmethod
+    def bclconvert_cmd_generation_service(config):
+        """
+        Create a command generation service unless one already exists.
+        """
+        if BclConvertServiceMixin._bclconvert_cmd_generation_service:
+            return BclConvertServiceMixin._bclconvert_cmd_generation_service
+        else:
+            BclConvertServiceMixin._bclconvert_cmd_generation_service = BclConvertRunnerFactory(config)
+            return BclConvertServiceMixin._bclconvert_cmd_generation_service
+
+class BaseBclConvertHandler(BaseRestHandler):
+    """
+    Base handler for bclconvert.
+    """
+
+    def initialize(self, config):
+        """
+        Ensures that any parameters feed to this are available
+        to subclasses.
+        """
+        self.config = config
+        self.bclconvert_log_file_provider = BclConvertLogFileProvider(self.config)
+
+
+class VersionsHandler(BaseBclConvertHandler):
+    """
+    Get the available bclconvert versions that the
+    service knows about.
+    """
+
+    def get(self):
+        """
+        Returns all available bclconvert versions (as defined by config).
+        """
+        available_versions = self.config["bclconvert"]["versions"]
+        self.write_object(available_versions)
+
+class StartHandler(BaseBclConvertHandler, BclConvertServiceMixin):
+    """
+    Start bclconvert
+    """
+
+    def create_config_from_request(self, runfolder, request_body):
+        """
+        For the specified runfolder, will look it up from the place setup in the
+        configuration, and then parse additional data from the request_data object.
+        This can be used to override any default setting in the resulting bclconvertConfig
+        instance.
+        :param runfolder: name of the runfolder we want to create a config for
+        :param request_body: the body of the request. Can be empty, in which case if will not be loaded.
+        :return: an instances of bclconvertConfig
+        """
+
+        if request_body:
+            request_data = json.loads(request_body)
+        else:
+            request_data = {}
+
+        # TODO Make sure to escape them for sec. reasons.
+        bclconvert_version = ""
+        runfolder_input = ""
+        samplesheet = ""
+        output = ""
+        barcode_mismatches = ""
+        tiles = ""
+        exclude_tiles = ""
+        use_base_mask = ""
+        bcl_only_lane = None
+        create_indexes = False
+        bclconvert_bcl_num_parallel_tiles = None
+        bclconvert_bcl_num_conversion_threads = None
+        bclconvert_bcl_num_compression_threads = None
+        bclconvert_bcl_num_decompression_threads = None
+        additional_args = ""
+
+        runfolder_base_path = self.config["runfolder_path"]
+        runfolder_input = os.path.join(runfolder_base_path, runfolder)
+
+        if not os.path.isdir(runfolder_input):
+            raise ArteriaUsageException(f"No such file: {runfolder_input}")
+
+        if "bclconvert_version" in request_data:
+            bclconvert_version = request_data["bclconvert_version"]
+
+        if "output" in request_data:
+            output = request_data["output"]
+
+        if "samplesheet" in request_data:
+            samplesheet = request_data["samplesheet"]
+
+        if "barcode_mismatches" in request_data:
+            barcode_mismatches = request_data["barcode_mismatches"]
+
+        if "tiles" in request_data:
+            tiles = request_data["tiles"]
+
+        if "exclude_tiles" in request_data:
+            exclude_tiles = request_data["exclude_tiles"]
+
+        if "use_base_mask" in request_data:
+            use_base_mask = request_data["use_base_mask"]
+
+        if "create_indexes" in request_data:
+            if request_data["create_indexes"] == "True":
+                create_indexes = True
+
+        if "bcl_only_lane" in request_data:
+            bcl_only_lane = request_data['bcl_only_lane']
+
+        if "bclconvert_bcl_num_parallel_tiles" in request_data:
+            bclconvert_bcl_num_parallel_tiles = request_data['bclconvert_bcl_num_parallel_tiles']
+
+        if "bclconvert_bcl_num_conversion_threads" in request_data:
+            bclconvert_bcl_num_conversion_threads = request_data["bclconvert_bcl_num_conversion_threads"]
+
+        if "bclconvert_bcl_num_compression_threads" in request_data:
+            bclconvert_bcl_num_compression_threads = request_data["bclconvert_bcl_num_compression_threads"]
+
+        if "bclconvert_bcl_num_decompression_threads" in request_data:
+            bclconvert_bcl_num_decompression_threads = request_data["bclconvert_bcl_num_decompression_threads"]
+
+        if "additional_args" in request_data:
+            additional_args = request_data["additional_args"]
+
+        config = BclConvertConfig(
+            general_config=self.config,
+            bclconvert_version=bclconvert_version,
+            runfolder_input=runfolder_input,
+            output=output,
+            samplesheet=samplesheet,
+            barcode_mismatches=barcode_mismatches,
+            tiles=tiles,
+            exclude_tiles=exclude_tiles,
+            use_base_mask=use_base_mask,
+            create_indexes=create_indexes,
+            bclconvert_bcl_num_parallel_tiles=bclconvert_bcl_num_parallel_tiles,
+            bclconvert_bcl_num_conversion_threads=bclconvert_bcl_num_conversion_threads,
+            bclconvert_bcl_num_compression_threads=bclconvert_bcl_num_compression_threads,
+            bclconvert_bcl_num_decompression_threads=bclconvert_bcl_num_decompression_threads,
+            additional_args=additional_args)
+
+        return config
+
+    def post(self, runfolder):
+        """
+        Starts a bclconvert for a runfolder. The input data can contain extra
+        parameters for bclconvert. It should be a json encoded object and
+        can contain one or more of the following parameters:
+         - bclconvert_version
+         - output
+         - samplesheet (provide the entire samplesheet in the request)
+         - barcode_mismatches
+         - tiles
+         - use_base_mask
+         - additional_args
+        If these are not set defaults setup in bclconvertConfig will be
+        used (and those should be good enough for most cases).
+
+        :param runfolder: name of the runfolder we want to start bclconvert for
+        """
+
+        try:
+            runfolder_config = self.create_config_from_request(runfolder, self.request.body)
+
+            job_runner = self.bclconvert_cmd_generation_service(self.config). \
+                create_bclconvert_runner(runfolder_config)
+            bclconvert_version = job_runner.version()
+            cmd = job_runner.construct_command()
+            # If the output directory exists, we always want to clear it.
+            job_runner.delete_output()
+            job_runner.symlink_output_to_unaligned()
+
+            log_file = self.bclconvert_log_file_provider.log_file_path(runfolder)
+
+            job_id = self.runner_service().start(
+                cmd,
+                nbr_of_cores=runfolder_config.nbr_of_cores,
+                run_dir=runfolder_config.runfolder_input,
+                stdout=log_file,
+                stderr=log_file)
+
+            log.info(
+                f"Cmd: {cmd} started in {runfolder_config.runfolder_input} "
+                f"with {runfolder_config.nbr_of_cores} cores. Writing logs to: {log_file}")
+
+            reverse_url = self.reverse_url("status", job_id)
+            status_end_point = f"{self.request.protocol}://{self.request.host}{reverse_url}"
+
+            response_data = {
+                "job_id": job_id,
+                "bclconvert_version": bclconvert_version,
+                "service_version": version,
+                "link": status_end_point,
+                "state": State.STARTED}
+
+            self.set_status(202, reason="started processing")
+            self.write_json(response_data)
+        except ArteriaUsageException as e:
+            log.warning(f"Failed starting {runfolder}. Message: {e}")
+            self.send_error(status_code=500, reason=e)
+
+
+
+class StatusHandler(BaseBclConvertHandler, BclConvertServiceMixin):
+    """
+    Get the status of one or all jobs.
+    """
+
+    def get(self, job_id):
+        """
+        Get the status of the specified job_id, or if now id is given, the
+        status of all jobs.
+        :param job_id: to check status for (set to empty to get status for all)
+        """
+
+        if job_id:
+            status = {"state": self.runner_service().status(job_id)}
+        else:
+            all_status = self.runner_service().status_all()
+            status_dict = {}
+            for k,v in all_status.iteritems():
+                status_dict[k] = {"state": v}
+            status = status_dict
+
+        self.write_json(status)
+
+
+class StopHandler(BaseBclConvertHandler, BclConvertServiceMixin):
+    """
+    Stop one or all jobs.
+    """
+
+    def post(self, job_id):
+        """
+        Stops the job with the specified id.
+        :param job_id: of job to stop, or set to "all" to stop all jobs
+        """
+        try:
+            if job_id == "all":
+                log.info("Attempting to stop all jobs.")
+                self.runner_service().stop_all()
+                log.info("Stopped all jobs!")
+                self.set_status(200)
+            elif job_id:
+                log.info(f"Attempting to stop job: {job_id}")
+                self.runner_service().stop(job_id)
+                self.set_status(200)
+            else:
+                ArteriaUsageException("Unknown job to stop")
+        except ArteriaUsageException as e:
+            log.warning(f"Failed stopping job: {job_id}. Message: {e}")
+            self.send_error(500, reason=str(e())
+
+
+class BclConvertLogHandler(BaseBclConvertHandler):
+    """
+    Gets the content of the log for a particular runfolder
+    """
+
+    def get(self, runfolder):
+        """
+        Get the content of the log for a particular runfolder
+        :param runfolder:
+        :return:
+        """
+        try:
+            log_content = self.bclconvert_log_file_provider.get_log_for_runfolder(runfolder)
+            response_data = {"runfolder": runfolder, "log": log_content}
+            self.set_status(200)
+            self.write_json(response_data)
+        except IOError as e:
+            log.warning(f"Problem with accessing {runfolder}, message: {e}")
+            self.send_error(500, reason=str(e))
---
+++ tests/tests_bclconvert_utils.py
@@ -0,0 +1,229 @@
+# This file has been modified from the https://github.com/arteria-project/arteria-bcl2fastq repo
+# bcl2fastq/tests/test_bcl2fastq_utils_logs.py
+
+import unittest
+from mock import patch
+import tempfile
+import os
+
+from bcl2fastq.lib.bcl2fastq_utils import *
+from bcl2fastq.lib.illumina import Samplesheet
+from test_utils import TestUtils, DummyConfig
+
+
+DUMMY_CONFIG = DummyConfig()
+
+class TestBclConvertConfig(unittest.TestCase):
+
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+    samplesheet_file = test_dir + "/sampledata/new_samplesheet_example.csv"
+    samplesheet_with_no_tag = test_dir + "/sampledata/no_tag_samplesheet_example.csv"
+    dummy_config = DummyConfig()
+
+    def test_get_bclconvert_version_from_run_parameters(self):
+        runfolder = TestBclConvertConfig.test_dir + "/sampledata/HiSeq-samples/2014-02_13_average_run"
+        version = BclConvertConfig.get_bcl2fastq_version_from_run_parameters(runfolder, self.dummy_config)
+        self.assertEqual(version, "4.0.3")
+
+    def test_is_single_read_true(self):
+        runfolder = TestBclConvertConfig.test_dir + "/sampledata/HiSeq-samples/2014-02_13_average_run"
+        self.assertFalse(BclConvertConfig.is_single_read(runfolder))
+
+    def test_is_single_read_false(self):
+        runfolder = TestBclConvertConfig.test_dir + "/sampledata/MiSeq-samples/2014-02_11_50kit_single_read"
+        self.assertTrue(BclConvertConfig.is_single_read(runfolder))
+
+    def test_get_length_of_indexes(self):
+        runfolder = TestBclConvertConfig.test_dir + "/sampledata/HiSeq-samples/2014-02_13_average_run"
+        index_and_length = BclConvertConfig.get_length_of_indexes(runfolder)
+        self.assertEqual(index_and_length, {2: 7})
+
+    def test_write_samplesheet(self):
+        f = tempfile.mktemp()
+        BclConvertConfig.write_samplesheet(TestUtils.DUMMY_SAMPLESHEET_STRING, f)
+        with open(f, "r") as my_file:
+            content = my_file.read()
+            self.assertEqual(content, TestUtils.DUMMY_SAMPLESHEET_STRING)
+        os.remove(f)
+
+    def test_samplesheet_gets_written(self):
+        with patch.object(BclConvertConfig, "write_samplesheet") as ws:
+            # If we provide a samplesheet to the config this should be written.
+            # In this case this write call is mocked away to make testing easier,
+            # but the write it self should be trivial.
+            config = BclConvertConfig(
+                general_config = DUMMY_CONFIG,
+                bcl2fastq_version = "4.0.3",
+                runfolder_input = "test/runfolder",
+                output = "test/output",
+                samplesheet=TestUtils.DUMMY_SAMPLESHEET_STRING)
+
+            ws.assert_called_once_with(TestUtils.DUMMY_SAMPLESHEET_STRING, config.samplesheet_file)
+
+    def test_get_bases_mask_per_lane_from_samplesheet(self):
+        mock_read_index_lengths = {2: 9, 3: 9}
+        expected_bases_mask = {1: "y*,i8n*,i8n*,y*",
+                               2: "y*,i6n*,n*,y*",
+                               3: "y*,i6n*,n*,y*",
+                               4: "y*,i7n*,n*,y*",
+                               5: "y*,i7n*,n*,y*",
+                               6: "y*,i7n*,n*,y*",
+                               7: "y*,i7n*,n*,y*",
+                               8: "y*,i7n*,n*,y*",
+                               }
+        samplesheet = Samplesheet(TestBclConvertConfig.samplesheet_file)
+        actual_bases_mask = BclConvertConfig. \
+            get_bases_mask_per_lane_from_samplesheet(samplesheet, mock_read_index_lengths, False)
+        self.assertEqual(expected_bases_mask, actual_bases_mask)
+
+    def test_get_bases_mask_per_lane_from_samplesheet_single_read(self):
+        mock_read_index_lengths = {2: 9, 3: 9}
+        expected_bases_mask = {1: "y*,i8n*,i8n*",
+                               2: "y*,i6n*,n*",
+                               3: "y*,i6n*,n*",
+                               4: "y*,i7n*,n*",
+                               5: "y*,i7n*,n*",
+                               6: "y*,i7n*,n*",
+                               7: "y*,i7n*,n*",
+                               8: "y*,i7n*,n*",
+                               }
+        samplesheet = Samplesheet(TestBclConvertConfig.samplesheet_file)
+        actual_bases_mask = BclConvertConfig. \
+            get_bases_mask_per_lane_from_samplesheet(samplesheet, mock_read_index_lengths, True)
+        self.assertEqual(expected_bases_mask, actual_bases_mask)
+
+    def test_get_bases_mask_per_lane_from_samplesheet_invalid_length_combo(self):
+        # These are to short compared to the length indicated in the samplesheet
+        mock_read_index_lengths = {2: 4, 3: 4}
+        samplesheet = Samplesheet(TestBclConvertConfig.samplesheet_file)
+
+        with self.assertRaises(ArteriaUsageException):
+            BclConvertConfig. \
+                get_bases_mask_per_lane_from_samplesheet(samplesheet, mock_read_index_lengths, False)
+
+    def test_get_bases_mask_per_lane_from_samplesheet_no_tag(self):
+        # If we don't have tag for one lane in the samplesheet.
+        mock_read_index_lengths = {2: 6}
+        expected_bases_mask = {1: "y*,n*,y*",
+                               2: "y*,n*,y*",
+                               3: "y*,i6,y*",
+                               4: "y*,i6,y*",
+                               5: "y*,i6,y*",
+                               6: "y*,i6,y*",
+                               7: "y*,n*,y*",
+                               8: "y*,n*,y*",
+                               }
+        samplesheet = Samplesheet(TestBclConvertConfig.samplesheet_with_no_tag)
+        actual_bases_mask = BclConvertConfig. \
+            get_bases_mask_per_lane_from_samplesheet(samplesheet, mock_read_index_lengths, False)
+        self.assertEqual(expected_bases_mask, actual_bases_mask)
+
+
+class TestBCLConvertRunnerFactory(unittest.TestCase):
+
+    dummy_config = DummyConfig()
+
+    def test_create_bcl2fastq1x_runner(self):
+        config = BclConvertConfig(
+            general_config = DUMMY_CONFIG,
+            bcl2fastq_version = "4.0.3",
+            runfolder_input = "test/runfolder",
+            output = "test/output")
+
+        factory = BCL2FastqRunnerFactory(self.dummy_config)
+        runner = factory.create_bcl2fastq_runner(config)
+        self.assertIsInstance(runner, BCL2Fastq1xRunner)
+
+    def test_create_bcl2fastq2x_runner(self):
+        config = BclConvertConfig(
+            general_config = DUMMY_CONFIG,
+            bcl2fastq_version = "4.0.3",
+            runfolder_input = "test/runfolder",
+            output = "test/output")
+
+        factory = BCL2FastqRunnerFactory(self.dummy_config)
+        runner = factory.create_bcl2fastq_runner(config)
+        self.assertIsInstance(runner, BCLConvertRunner, msg="runner is: " + str(runner))
+
+    def test_create_invalid_version_runner(self):
+        config = BclConvertConfig(
+            general_config = DUMMY_CONFIG,
+            bcl2fastq_version = "1.7",
+            runfolder_input = "test/runfolder",
+            output = "test/output")
+
+        factory = BCL2FastqRunnerFactory(self.dummy_config)
+        with self.assertRaises(LookupError):
+            factory.create_bcl2fastq_runner(config)
+
+
+class TestConvertRunner(unittest.TestCase):
+    def test_construct_command(self):
+
+        config = BclConvertConfig(
+            general_config = DUMMY_CONFIG,
+            bcl2fastq_version = "4.0.3",
+            runfolder_input = "test/runfolder",
+            output = "test/output",
+            barcode_mismatches = "2",
+            tiles="s1,s2,s3",
+            use_base_mask="--use-bases-mask y*,i6,i6,y* --use-bases-mask 1:y*,i5,i5,y*",
+            additional_args="--my-best-arg 1 --my-best-arg 2")
+
+        runner = BCLConvertRunner(config, "/bcl/binary/path")
+        command = runner.construct_command()
+        expected_command = "/bcl/binary/path --input-dir test/runfolder/Data/Intensities/BaseCalls " \
+                           "--output-dir test/output " \
+                           "--sample-sheet test/runfolder/SampleSheet.csv " \
+                           "--barcode-mismatches 2 " \
+                           "--tiles s1,s2,s3 " \
+                           "--use-bases-mask y*,i6,i6,y* --use-bases-mask 1:y*,i5,i5,y* " \
+                           "--my-best-arg 1 --my-best-arg 2"
+        self.assertEqual(command, expected_command)
+
+class TestBCLConvertRunner(unittest.TestCase):
+
+    config = BclConvertConfig(
+        general_config = DUMMY_CONFIG,
+        bcl2fastq_version = "4.0.3",
+        runfolder_input = "test/runfolder",
+        output = "test/output",
+        barcode_mismatches = "2",
+        tiles="s1,s2,s3",
+        use_base_mask="--use-bases-mask y*,i6,i6,y* --use-bases-mask 1:y*,i5,i5,y*",
+        additional_args="--my-best-arg 1 --my-best-arg 2")
+
+    class DummyBCLConvertRunner(BCL2FastqRunner):
+        def __init__(self, config, binary, dummy_command):
+            self.dummy_command = dummy_command
+            BCL2FastqRunner.__init__(self, config, binary)
+
+        def construct_command(self):
+            return self.dummy_command
+
+    def test_symlink_output_to_unaligned(self):
+        with patch.object(os, 'symlink', return_value=None) as m:
+            dummy_runner = self.DummyBCLConvertRunner(TestBCLConvertRunner.config, None, None)
+            dummy_runner.symlink_output_to_unaligned()
+            m.assert_called_with(
+                TestBCLConvertRunner.config.output,
+                TestBCLConvertRunner.config.runfolder_input + "/Unaligned")
+
+        # Check that trying to create an already existing softlink doesn't break the function.
+        # Please note that the second side effect is None, which means that the second time th
+        # mock is called it will work.
+        with patch.object(os, 'remove', return_value=None) as r, \
+             patch.object(os, 'symlink', side_effect=[OSError(17, "message"), None]) as m:
+            dummy_runner = self.DummyBCLConvertRunner(TestBCLConvertRunner.config, None, None)
+            dummy_runner.symlink_output_to_unaligned()
+            m.assert_called_with(TestBCLConvertRunner.config.output,
+                                 TestBCLConvertRunner.config.runfolder_input + "/Unaligned")
+            r.assert_called_with(TestBCLConvertRunner.config.runfolder_input + "/Unaligned")
+
+        # While any other error should
+        with patch.object(os, 'symlink', side_effect=OSError(1, "message")) as m:
+            with self.assertRaises(OSError):
+                dummy_runner = self.DummyBCLConvertRunner(TestBCLConvertRunner.config, None, None)
+                dummy_runner.symlink_output_to_unaligned()
+                m.assert_called_with(TestBCLConvertRunner.config.output,
+                                     TestBCLConvertRunner.config.runfolder_input + "/Unaligned")
